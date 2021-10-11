44cce62042ab2e2d5ccd083dc6f9fc044f920d47
spammy <spammy7147@gmail.com>
2021-10-11 20:10:40 +0900

    Climbing Stairs Contains Duplicate Sum of Two Inegers

---
+++ spammy7147/week1/371. Sum of Two Integers.java
@@ -0,0 +1,13 @@
+class Solution371 {
+    public int getSum(int a, int b) {
+        return Integer.sum(a,b);
+    }
+}
+
+
+class Main371 {
+    public static void main(String[] args) {
+        System.out.println(new Solution371().getSum(1,2));
+
+    }
+}
