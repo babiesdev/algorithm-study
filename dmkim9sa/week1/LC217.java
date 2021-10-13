627615d2fd5a63548c3692e775caae87fb6c543a
dongmin <dmkim9sa@gmail.com>
2021-10-13 15:27:16 +0900

    [LeetCode, 217] Contains Duplicate 풀이 추가

--- dmkim9sa/week1/LC217.java
+++ dmkim9sa/week1/LC217.java
@@ -1,10 +1,34 @@
 package leetcode.week1;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 // Contains Duplicate [lv: easy]
 public class LC217 {
 
+    // Set 1
+    public boolean containsDuplicateSet1(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        return !(set.size() == nums.length);
+    }
+
+    // Set 2
+    public boolean containsDuplicateSet2(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+
+    // 배열 정렬
     public boolean containsDuplicate(int[] nums) {
 
         Arrays.sort(nums);
