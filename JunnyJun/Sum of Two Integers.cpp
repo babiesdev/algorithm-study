18fc8ac0b6de0fea209c9c05eed4d35a98ce516e
Lee-ChangHee <chbe5082@naver.com>
2021-10-17 11:07:26 +0900

    fix

--- JunnyJun/Sum of Two Integers.cpp
+++ JunnyJun/Sum of Two Integers.cpp
@@ -3,22 +3,13 @@ class Solution
 public:
     int getSum(int a, int b)
     {
-        if (a == 0)
-        {
-            return b;
-        }
-        if (b == 0)
-        {
-            return a;
-        }
-
+        int carry = 0;
         while (b != 0)
         {
-
-            int bitOperator = a & b;
+            carry = a & b;
             a = a ^ b;
             b = carry << 1;
         }
-        return bitOperator;
+        return a;
     }
 };
