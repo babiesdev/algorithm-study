d62566be89cb1b5063f60a760d62155d22203d63
kimdainn <dainkim@sparrowfasoo.com>
2021-10-31 15:30:17 +0900

    Q191_NumberOf1Bits

--- doongjeol/leetcode/binary/Q191_NumberOf1Bits.java
+++ doongjeol/leetcode/binary/Q191_NumberOf1Bits.java
@@ -1,4 +1,22 @@
 package leetcode.binary;
 
 public class Q191_NumberOf1Bits {
+    public static int hammingWeight(int n) {
+        int answer = 0 ;
+        int mask = 1;
+
+        for (int i = 0; i < 32; i++) {
+            if((n & mask) != 0){
+                answer++;
+            }
+            mask <<= 1; // mask = mask << 1
+        }
+
+        return answer;
+
+    }
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(hammingWeight(n));
+    }
 }
