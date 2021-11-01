175852dab4b92efe67c8840df33e304bd5a60606
100milliongold <gadian88@gmail.com>
2021-11-01 12:27:59 +0900

    141. Linked List Cycle

--- 100milliongold/leetcode/interval/56.Merge Intervals.js
+++ 100milliongold/leetcode/interval/56.Merge Intervals.js
@@ -8,8 +8,7 @@ var merge = function(intervals) {
     // 다음 간격의 시작 시간으로 이동하면 다음 간격이 있음을 알 수 있습니다.
     // 겹치고 병합합니다.
     // 겹침이 없으면 다음 간격을 추가합니다.
-    // 출력 배열의 간격 목록을 만들고 프로세스를 반복합니다.
-    // 전체 구간 목록을 살펴볼 때까지.
+    // 전체 구간 목록을 살펴볼 때까지 출력 배열의 간격 목록을 만들고 프로세스를 반복합니다.
     for(let i = 1; i< intervals.length;i++) {
         const next = intervals[i]
         if(current[1] >= next[0]) {
