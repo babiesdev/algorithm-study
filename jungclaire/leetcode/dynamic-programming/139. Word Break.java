e9302a8754ccf9fdd582f5ebccfc1e67e9b047af
Claire Jung <dev.jungclaire@gmail.com>
2021-11-27 20:29:30 +0900

    Word Break

---
+++ jungclaire/leetcode/dynamic-programming/139. Word Break.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.List;
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> hs = new HashSet<>(wordDict);
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && hs.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
