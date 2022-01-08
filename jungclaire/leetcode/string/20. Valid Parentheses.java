803bb9b0620833e3b4bd23ffdf7d68e4ba245eb2
Claire Jung <dev.jungclaire@gmail.com>
2022-01-08 21:40:20 +0900

    Valid Parentheses

---
+++ jungclaire/leetcode/string/20. Valid Parentheses.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
