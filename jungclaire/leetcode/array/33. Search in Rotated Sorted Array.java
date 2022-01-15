c66f555de2d155cb798b83d047c5fee08e0ccf6a
Claire Jung <83348294+jungclaire@users.noreply.github.com>
2022-01-15 23:16:14 +0900

    Search in Rotated Sorted Array

---
+++ jungclaire/leetcode/array/33. Search in Rotated Sorted Array.java
@@ -0,0 +1,26 @@
+class Solution33 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[left] <= nums[mid]) {
+                if (nums[mid] < target || nums[left] > target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (target < nums[mid] || target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
